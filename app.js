a37729e4addd7edc9e46c662bb926fcb542fe37e
Alexander Stekolshchikov <alexstekk@yandex.ru>
2023-03-01 17:54:28 +0300

    done

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const products = [
 	{
-		 id: 1,
-		 title: 'Lenovo Yoga',
-		 price: 3000,
+		id: 1,
+		title: 'Lenovo Yoga',
+		price: 3000,
 	},
 	{
-		 id: 2,
-		 title: 'Acer Aspire',
-		 price: 1800,
+		id: 2,
+		title: 'Acer Aspire',
+		price: 1800,
 	},
 	{
-		 id: 3,
-		 title: 'Dell Vostro',
-		 price: 3400
+		id: 3,
+		title: 'Dell Vostro',
+		price: 3400,
 	},
 ];
 
@@ -20,9 +20,12 @@ let order = [];
 
 function addToBasket(productId) {
 	// TODO: добавить проверку наличия товара в заказе (при наличии выдать alert, что товар уже в корзине)
-
+	if (order.find((item) => item.id === productId)) {
+		return alert('товар уже в корзине');
+	}
 	// TODO: если товар еще не в корзине, добавить его из массива products
-
+	const currentProduct = products.find((item) => item.id === productId);
+	order = [...order, currentProduct];
 	// Эти строчки не трогаем, они отвечают за переотрисовку страницы
 	renderCart();
 	rerenderTotalPrice();
@@ -30,17 +33,17 @@ function addToBasket(productId) {
 
 function removeFromBasket(productId) {
 	// TODO: описать логику удаления товара из корзины
-
+	order = order.filter(item => item.id !== productId);
 	// Эти строчки не трогаем, они отвечают за переотрисовку страницы
 	renderCart();
 	rerenderTotalPrice();
 }
 
-
 function rerenderTotalPrice() {
 	// TODO: опишите функционал подсчета общей стоимости заказа
-
-
+	const totalPrice = order.reduce((acc, item) => {
+		return acc + item.price
+	}, 0);
 	// Не меняйте эту строчку
 	document.getElementById('total').innerText = totalPrice;
 }
@@ -50,10 +53,10 @@ function renderCart() {
 	const cart = document.getElementById('basket-items');
 
 	cart.innerHTML = '';
-	order.forEach(item => {
-		 const el = document.createElement('li');
-		 el.innerText = item.title;
-		 el.onclick = () => removeFromBasket(item.id);
-		 cart.appendChild(el);
-	})
+	order.forEach((item) => {
+		const el = document.createElement('li');
+		el.innerText = item.title;
+		el.onclick = () => removeFromBasket(item.id);
+		cart.appendChild(el);
+	});
 }
